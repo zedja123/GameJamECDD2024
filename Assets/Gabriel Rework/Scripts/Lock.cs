d8cb9bd9b833a2fe63bfffb0956a25bffaf56990
Gabriel Marroso <gabrielmarroso@gmail.com>
2024-07-20 16:30:29 -0300

    Lock prefab

---
+++ Assets/Gabriel Rework/Scripts/Lock.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lock : MonoBehaviour
+{
+    [SerializeField] private EnemyMaster enemyMaster;
+
+    private void Start()
+    {
+        enemyMaster = GetComponent<EnemyMaster>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (enemyMaster.health <= 1)
+        {
+            SceneManager.LoadScene("VictoryScene");
+
+        }
+    }
+}
